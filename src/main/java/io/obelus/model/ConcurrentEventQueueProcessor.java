c8fd10822a132e7708f5cdd36d264e644989c97e
Juan Ortiz <jportiz32@gmail.com>
2020-01-02 09:42:31 -0500

    Implement ConcurrentEventQueueProcessor

---
+++ src/main/java/io/obelus/model/ConcurrentEventQueueProcessor.java
@@ -0,0 +1,35 @@
+package io.obelus.model;
+
+import io.obelus.model.converter.JsonMarshaller;
+
+/**
+ * This class is used to transform an entire {@link io.obelus.model.ConcurrentEventQueue} into a single JSON object.
+ *
+ * <p>{@link io.obelus.model.ObelusAnalytics} uses this object to compress queued events that will eventually be sent
+ * to the Obelus servers.</p>
+ */
+class ConcurrentEventQueueProcessor {
+
+    private JsonMarshaller jsonMarshaller;
+
+    /**
+     * Creates a new concurrent event queue processor.
+     *
+     * @param jsonMarshaller
+     *         the {@link io.obelus.model.converter.JsonMarshaller} implementation to use.
+     */
+    ConcurrentEventQueueProcessor(JsonMarshaller jsonMarshaller) {
+        this.jsonMarshaller = jsonMarshaller;
+    }
+
+    /**
+     * Converts a {@link io.obelus.model.ConcurrentEventQueue} into a single JSON object.
+     */
+    protected synchronized String process(ConcurrentEventQueue repository) {
+        if (repository.get().isEmpty()) {
+            return null;
+        }
+
+        return jsonMarshaller.toJson(repository);
+    }
+}
